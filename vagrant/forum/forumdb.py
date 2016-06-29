03e1f616aade935783b9ec32717eba8424392927
SvrAdityaReddy <aditya.seelapureddy@yahoo.com>
2016-06-29 15:42:28 +0530

    curing bobby tables

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -6,7 +6,7 @@ import time
 import psycopg2
 
 ## Database connection
-DB = psycopg2.connect("dbname=forum");
+#DB = psycopg2.connect("dbname=forum");
 i=0
 #cur = DB.cursor();
 
@@ -21,9 +21,11 @@ def GetAllPosts():
     '''
     #posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
     #posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum");
     cur=DB.cursor();
     cur.execute("SELECT content,time from posts order by time Desc");
     rows = cur.fetchall();
+    DB.close();
     posts = [{'content': str(row[0]), 'time': str(row[1])} for row in rows]
     return posts
 
@@ -34,12 +36,14 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
+    DB = psycopg2.connect("dbname=forum");
     cur = DB.cursor();
     t = time.strftime('%c', time.localtime())
     global i
     #type(t);
-    cur.execute("INSERT INTO posts (content,time,id) VALUES('%s','%s',%s)" % (content,t,i+1));
+    cur.execute("INSERT INTO posts (content,time,id) VALUES(%s,%s,%s)" , (content,t,i+1,));
     DB.commit();
     i=i+1;
+    DB.close();
     #t = time.strftime('%c', time.localtime())
     #DB.append((t, content))
