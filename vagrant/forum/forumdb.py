ec5a92751763ffcec5d6b610d458c0163b6b1b9b
SvrAdityaReddy <aditya.seelapureddy@yahoo.com>
2016-06-29 15:19:45 +0530

    adding database named forum

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,9 +3,12 @@
 # 
 
 import time
+import psycopg2
 
 ## Database connection
-DB = []
+DB = psycopg2.connect("dbname=forum");
+i=0
+#cur = DB.cursor();
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +19,12 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    #posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    cur=DB.cursor();
+    cur.execute("SELECT content,time from posts order by time Desc");
+    rows = cur.fetchall();
+    posts = [{'content': str(row[0]), 'time': str(row[1])} for row in rows]
     return posts
 
 ## Add a post to the database.
@@ -27,5 +34,12 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
+    cur = DB.cursor();
     t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    global i
+    #type(t);
+    cur.execute("INSERT INTO posts (content,time,id) VALUES('%s','%s',%s)" % (content,t,i+1));
+    DB.commit();
+    i=i+1;
+    #t = time.strftime('%c', time.localtime())
+    #DB.append((t, content))
